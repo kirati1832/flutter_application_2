999e53761f16f6012c193042e643df017eab2046
kirati1832 <kirati.1832@gmail.com>
2024-12-16 09:36:03 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,151 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           useMaterial3: true,
-          appBarTheme: AppBarTheme(color: Color.fromARGB(255, 0, 255, 234))),
+          appBarTheme: AppBarTheme(color: Color.fromARGB(255, 86, 1, 88)),
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 255, 0, 0))),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('AppBar title'),
+        appBar: AppBar(centerTitle: true,
+          title: Image.asset('assets/images/unnamed.png',height: 100,),
+          leading: IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                debugPrint('leading icon');
+              }),
         ),
-        body: Text('Hello World'),
+        body: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/unnamed.png",
+              height: 250,
+            ),
+            const Text(
+              'ขอบคุณที่ใช้บริการ',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              'เรายินดีที่ได้เป็นส่วนหนึ่งในการเดินทางของคุณ\n\n',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            const Row(
+              children: [
+                const Text('สรุปรายละเอียดการชาร์จ',
+                    style:
+                    TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+              ],
+            ),
+            SizedBox(height: 10),
+            const Row(
+              children: [
+                Icon(Icons.calendar_month),
+                Text(
+                  ' วันที่ชาร์จ',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  '9 กันยายน 2566',
+                  style: TextStyle(
+                      fontSize: 18,fontWeight: FontWeight.bold
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            const Row(
+              children: [
+                Icon(Icons.charging_station),
+                Text(
+                  ' สถานีชาร์จ',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  'Pea Volta กรุงเทพ',
+                  style: TextStyle(
+                      fontSize: 18,fontWeight: FontWeight.bold
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            const Row(
+              children: [
+                Icon(Icons.electric_car),
+                Text(
+                  ' ประเภทหัวชาร์จ',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  'CCS2',
+                  style: TextStyle(
+                      fontSize: 18,fontWeight: FontWeight.bold
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            const Row(
+              children: [
+                Icon(Icons.punch_clock),
+                Text(
+                  ' เวลาการชาร์จ',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  '00:12:32',
+                  style: TextStyle(
+                      fontSize: 18,fontWeight: FontWeight.bold
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            const Row(
+              children: [
+                Icon(Icons.bolt),
+                Text(
+                  ' จำนวนหน่วย',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  '9.314 KWh',
+                  style: TextStyle(
+                      fontSize: 18,fontWeight: FontWeight.bold
+                  ),
+                ),
+              ],
+            ),
+
+
+          ],
+        ),
+
+
+        // body: const Column(
+        //   children: [
+        //     Text('Hello test CS KMUTNB',
+        //         style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        //     Text('My mobile app',
+        //         style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        //   ],
+        // ),
       ),
     );
   }
